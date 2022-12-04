942970c2b696d4b19cff3c87ab411f164c0c8c6b
Ferdinandy <ferdinandsc@yahoo.com.br>
2022-12-04 16:14:16 -0300

    Models included

--- recipes/urls.py
+++ recipes/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('contact/', views.contact),
     path('about/', views.about),
     path('recipes/<int:id>/', views.recipe, name="recipe"),
+    path('recipes/category/<int:category_id>/', views.category, name='category')
 ]
